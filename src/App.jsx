64cfcf807d5fe0d1a00a4c8ddfb8530cab173796
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-13 14:14:04 -0400

    feat: :sparkles: initial settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React
         </p>
         <a
           className="App-link"
