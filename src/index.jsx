e9d3f149d262d7919dbe0c5bd005fb554392bc00
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-14 15:35:20 -0400

    fix: :zap: index reformulation

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import Provider from './store/Provider';
+import Context from './store/Context';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <Provider store={Context}>
+      <App />
+    </Provider>
+  </HashRouter>,
   document.getElementById('root'),
 );
