acb9639c0fd146a9409fd5855194bcf1e0a15074
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-14 21:45:47 -0400

    fix: :fire: remove removeStorage Function

--- src/helpers/localStorage.js
+++ src/helpers/localStorage.js
@@ -7,9 +7,3 @@ export const setLocalStorage = (key, value) => {
 export const getStorage = (key, value) => {
   JSON.parse(localStorage.getItem(key, value));
 };
-
-// REMOVE LOCALSTORAGE
-export const removeStorage = (id, key) => {
-  const update = key.filter((product) => product.id !== id);
-  setLocalStorage('cart', update); return update;
-};
