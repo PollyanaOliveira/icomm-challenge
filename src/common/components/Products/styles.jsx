7c50645535aecfa9423e693924bc88456c45f886
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-15 22:45:22 -0400

    feat: :lipstick: create style component in Carousel

---
+++ src/common/components/Products/styles.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const widthScreen = styled.div`
+.widthFull {
+  width: 100vw;
+}
+`;
+
+export const Carousel = styled.div`
+.rec.rec-arrow {
+  height: 55px;
+  background-color: #C4C4C4;
+  color: #FFF;
+  border-radius: 0;
+}
+
+.rec.rec-arrow:hover {
+  background-color: #444444;
+}
+
+.rec.rec-dot {
+  background-color: #C4C4C4;
+  box-shadow: 0 0 1px 3px #C4C4C4;
+}
+
+.rec.rec-dot_active {
+  background-color: #444444;
+  box-shadow: 0 0 1px 3px #444444;
+}
+
+.sc-jSgvzq {
+  width: 100vw;
+}
+`;
