b61ad7e9bfd16535122721d6190a4cc6b1d92254
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-17 12:36:07 -0400

    feat: :white_check_mark: create Home test

---
+++ src/tests/Home.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { cleanup } from '@testing-library/react';
+
+import renderWithRouter from './helpers/renderWithRouter';
+
+import Home from '../common/pages/Home/Home';
+import Header from '../common/components/Header/Header';
+import FeaturedProducts from '../common/components/Products/FeaturedProducts';
+
+afterEach(cleanup);
+
+describe.only('Verifica elementos do Home', () => {
+  it('Verifica se há dois título "ÍRIS VERÃO 2022" e "LE LIS BLAC"', () => {
+    const { getByText } = renderWithRouter(
+      <Home>
+        <Header />
+        <FeaturedProducts />
+      </Home>,
+    );
+    const title1 = getByText(/ÍRIS VERÃO 2022/i);
+    const title2 = getByText(/LE LIS BLAC/i);
+    expect(title1).toBeInTheDocument();
+    expect(title2).toBeInTheDocument();
+  });
+});
