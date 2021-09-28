c62a886e72a49028b6eb2f4bd7bcd6a1bfa5d646
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-28 17:50:53 -0400

    fix: :zap: adjust clearFavorite function

--- src/store/Provider.jsx
+++ src/store/Provider.jsx
@@ -62,6 +62,7 @@ export default function Provider({ children }) {
 
   const clearFavorite = () => {
     setItemsFavorite([]);
+    setLocalStorage(LSFavorite, []);
   };
 
   const clearCart = () => {
