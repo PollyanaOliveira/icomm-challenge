a87baeb46a75965f91088a0a181f4ca670162f3a
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-29 11:37:10 -0400

    fix: :lipstick: adjust home css

--- src/common/pages/Home/Home.jsx
+++ src/common/pages/Home/Home.jsx
@@ -13,21 +13,23 @@ export default function Home() {
     <main>
       <Header />
       <section>
-        <section className="featured__images">
-          <section className="left__content">
-            <section className="title__images">
-              <p>ÍRIS</p>
-              <p>VERÃO</p>
-              <p>2022</p>
+        <section className="featured__container">
+          <section className="featured__images">
+            <section className="left__content">
+              <section className="title__images">
+                <p>ÍRIS</p>
+                <p>VERÃO</p>
+                <p>2022</p>
+              </section>
+              <img src={image5} alt="Destaque Verão" />
             </section>
-            <img src={image5} alt="Destaque Verão" />
-          </section>
-          <section className="right__content">
-            <section className="title__images">
-              <p>LE LIS</p>
-              <p>BLAC</p>
+            <section className="right__content">
+              <section className="title__images">
+                <p>LE LIS</p>
+                <p>BLAC</p>
+              </section>
+              <img src={image6} alt="Destaque Le Lis Blanc" />
             </section>
-            <img src={image6} alt="Destaque Le Lis Blanc" />
           </section>
         </section>
         <section className="featured__products">
